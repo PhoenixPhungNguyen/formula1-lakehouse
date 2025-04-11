e36e8dab98e2769ac57fb38542a009f9bd160b66
PhoenixPhungNguyen <PhoenixPhungNguyen>
2025-04-11 08:31:09 +0000

    Ingestion and Transformation

---
+++ ingestion/9.create_processed_database.sql
@@ -0,0 +1,11 @@
+-- Databricks notebook source
+CREATE DATABASE IF NOT EXISTS f1_processed
+LOCATION "/mnt/phoenixformula1dl /processed"
+
+-- COMMAND ----------
+
+DESC DATABASE f1_processed;
+
+-- COMMAND ----------
+
+
