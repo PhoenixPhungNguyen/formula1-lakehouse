bea4ac538171a427f60593785146b04f660442f9
PhoenixPhungNguyen <PhoenixPhungNguyen>
2025-04-12 22:24:38 +0000

    Improve the project to full load and incremental load

--- ingestion/9.create_processed_database.sql
+++ ingestion/9.create_processed_database.sql
@@ -1,6 +1,6 @@
 -- Databricks notebook source
 CREATE DATABASE IF NOT EXISTS f1_processed
-LOCATION "/mnt/phoenixformula1dl /processed"
+LOCATION "/mnt/formula1dl/processed"
 
 -- COMMAND ----------
 
